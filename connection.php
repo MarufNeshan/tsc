70881851630bf94e8b5b03cee9d758a97045a0ae
unknown <mdreazhosssainasif456@gmail.com>
2024-12-31 12:01:00 -0800

    login registation and dashboard task done

---
+++ connection.php
@@ -0,0 +1,12 @@
+
+<?php
+// Database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "tsc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+?>
--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-
-
 <?php
-// Database credentials
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "tsc";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully to the database.";
-}
-
-// Close connection
-$conn->close();
+include("./connection.php")
 ?>
 
 <!DOCTYPE html>
